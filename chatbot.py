fa05eff8c3694d857f227a9db7bdd8234a09555a
MaykonOliveiraC <maykonoliveiradev@gmail.com>
2023-10-26 20:51:53 -0300

    chatbot python

---
+++ chatbot.py
@@ -0,0 +1,31 @@
+import openai
+
+chave_api = "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"
+
+
+openai.api_key = chave_api
+
+def enviar_mensagem(mensagem, lista_mensagens=[]):
+    lista_mensagens.append({"role": "user", "content": mensagem})
+
+    resposta = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = lista_mensagens, 
+
+    )
+
+    return resposta["choices"][0]["message"]
+
+lista_mensagens = []   
+while True:
+    texto = input("Escreva aqui sua mensagem: ")
+
+    if texto == "sair":
+        break
+    else:
+        resposta = enviar_mensagem(texto)
+        lista_mensagens.append(resposta)
+        print("Chatbot:", resposta["content"])
+
+
+#print(enviar_mensagem("Em que ano einstein publicou a teoria geral da relatividade?"))
